437bec7b13b183e844b885cdee8a6ac787eeab11
Nguyễn Hoàng Nam <52146263+congviec18120062@users.noreply.github.com>
2019-11-03 08:32:42 +0700

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,20 @@ const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const csso = require('gulp-csso');
 const htmlmin = require('gulp-htmlmin');
+const clean = require('gulp-clean');
+const size = require('gulp-filesize');
 
 function style() {
   // 1. where is my sss file
   return src('./src/scss/**/*.scss')
     // 2. pass that file through compiler
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    // Minify css
+    .pipe(csso())
+    .pipe(rename({
+      suffix: '.min'
+    }))
     // 3. where do i save the complete css
     .pipe(dest('./src/css'))
   // 4. stream changes to all browser
@@ -33,30 +41,35 @@ function watch() {
 }
 
 function minifyImage() {
-  return src('./src/img/*')
+  return src(['./src/img/*', '!./src/img/**/*.min.*'])
     .pipe(image())
-    .pipe(dest('./docs/img'));
+    .pipe(rename({
+      //extname: '.min.js',
+      suffix: '.min'
+    }))
+    .pipe(dest('./docs/img'))
+    .pipe(dest('./src/img'))
+    .pipe(size());
 }
 
 function minifyJs() {
-  return src('./src/js/**/*js')
+  return src(['./src/js/**/*.js', '!./src/js/**/*.min.js'])
     .pipe(babel({
       presets: ['@babel/env']
     }))
     //.pipe(gulp.src('./src/vendor/*.js'))
     //.pipe(gulp.dest('./docs/js'))
     .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(dest('./docs/js'));
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(dest('./docs/js'))
+    .pipe(dest('./src/js'))
 }
 
 function minifyCss() {
-  return src('./src/css/**/*.css')
-    .pipe(autoprefixer())
-    // Minify css
-    .pipe(csso())
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(dest('./docs/css'));
+  return src(['./src/css/**/*.css'])
+    .pipe(dest('./docs/css'))
 }
 
 function minifyHtml() {
@@ -68,10 +81,20 @@ function minifyHtml() {
     .pipe(dest('./docs'))
 }
 
+function cleanDist() {
+  return src('./docs', { read: false })
+    .pipe(clean());
+}
+
 exports.style = style;
 exports.watch = watch;
 exports.minifyImage = minifyImage;
 exports.minifyJs = minifyJs;
 exports.minifyCss = minifyCss;
 exports.minifyHtml = minifyHtml;
-exports.default = series(minifyImage, parallel(minifyJs, parallel(minifyCss, minifyHtml)));
+exports.cleanDist = cleanDist;
+exports.default = series(cleanDist, parallel(
+  minifyImage, parallel(
+    minifyJs, parallel(minifyCss, minifyHtml)
+  )
+));
